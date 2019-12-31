aa49784f2d269035deb86bdc88fc6a6f3f869421
Victor Ayogu <victor@seriescode.com>
2019-12-31 02:43:21 +0200

    Add stories section

--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import story1 from "../statics/img/story-1.jpeg";
+import story2 from "../statics/img/story-2.jpeg";
 
 const Content = () => {
     return (
         <section className="story__content">
-            Story Content
+            <h3 className="heading-3 mb2">
+                Happy customers
+            </h3>
+            <h2 className="heading-2 heading-2--dark mb4">
+                &ldquo; the best decision of our lives &rdquo;
+            </h2>
+            <p className="story__text">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur distinctio necessitatibus pariatur
+                voluptatibus. Quidem consequatur harum volupta!
+            </p>
+
+            <button className="btn">
+                Find your own home
+            </button>
         </section>
     )
 };
@@ -11,7 +26,8 @@ const Content = () => {
 const Pictures = () => {
     return (
         <section className="story__pictures">
-            Story Pictures
+            <img src={story1} alt={""} className={"story__img--1"}/>
+            <img src={story2} alt={""} className={"story__img--2"}/>
         </section>
     )
 };
