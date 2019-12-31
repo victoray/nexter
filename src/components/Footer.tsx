b740920a81c9325c84e35dbcb35b54de2622d8d0
Victor Ayogu <victor@seriescode.com>
2019-12-31 05:24:40 +0200

    Add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 
-const Footer = ()=> {
+const Footer = () => {
     return (
         <footer className="footer">
-            Footer
+            <ul className="nav">
+                <li className="nav__item"><a href="#" className="nav__link">Find your dream home</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Request proposal</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Download Home planner</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Contact us</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Submit your property</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Come work with us</a></li>
+            </ul>
+
+            <p className="copyright">
+                &copy; Copyright 2020. Designed by Jason S. Built by Victor A.
+            </p>
         </footer>
     );
 };
--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,9 +1,52 @@
-import React from "react";
+import React, {Dispatch, useEffect, useRef, useState} from "react";
+import gal1 from "../statics/img/gal-1.jpeg";
+import gal2 from "../statics/img/gal-2.jpeg";
+import gal3 from "../statics/img/gal-3.jpeg";
+import gal4 from "../statics/img/gal-4.jpeg";
+import gal5 from "../statics/img/gal-5.jpeg";
+import gal6 from "../statics/img/gal-6.jpeg";
+import gal7 from "../statics/img/gal-7.jpeg";
+import gal8 from "../statics/img/gal-8.jpeg";
+import gal9 from "../statics/img/gal-9.jpeg";
+import gal10 from "../statics/img/gal-10.jpeg";
+import gal11 from "../statics/img/gal-11.jpeg";
+import gal12 from "../statics/img/gal-12.jpeg";
+import gal13 from "../statics/img/gal-13.jpeg";
+import gal14 from "../statics/img/gal-14.jpeg";
 
-const Gallery = ()=> {
+interface Image {
+    image: string;
+    position: number;
+}
+
+
+const Image = ({image, position}: Image) => {
+    return (
+
+        <figure className={"gallery__item gallery__item--" + position}>
+            <img src={image} alt="" className={"gallery__img"}/>
+        </figure>
+    )
+};
+
+const Gallery = () => {
     return (
         <section className="gallery">
-            Gallery
+            <Image image={gal1} position={1}/>
+            <Image image={gal2} position={2}/>
+            <Image image={gal3} position={3}/>
+            <Image image={gal4} position={4}/>
+            <Image image={gal5} position={5}/>
+            <Image image={gal5} position={6}/>
+            <Image image={gal6} position={7}/>
+            <Image image={gal7} position={8}/>
+            <Image image={gal8} position={9}/>
+            <Image image={gal9} position={10}/>
+            <Image image={gal10} position={11}/>
+            <Image image={gal11} position={12}/>
+            <Image image={gal12} position={13}/>
+            <Image image={gal13} position={14}/>
+            <Image image={gal14} position={15}/>
         </section>
     );
 };
