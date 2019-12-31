3cd099d0e343be699dd8d7b4acd25fe6812fc115
Victor Ayogu <victor@seriescode.com>
2019-12-31 15:32:41 +0200

    Add realtors section

--- src/components/Realtors.tsx
+++ src/components/Realtors.tsx
@@ -14,7 +14,7 @@ const Realtor = ({image, name, sold}: Realtor) => {
         <div className="realtor">
             <img src={image} alt="" className="realtor__img"/>
             <div className="realtor__details">
-                <h4 className="heading-4 heading4--light">
+                <h4 className="heading-4 heading-4--light">
                     {name}
                 </h4>
                 <p className="realtor__sold">{sold} houses sold</p>
@@ -27,9 +27,11 @@ const Realtors = () => {
     return (
         <section className="realtors">
             <h3 className="heading-3">Top 3 realtors</h3>
-            <Realtor image={realtor1} name={"Erik Holsgate"} sold={300}/>
-            <Realtor image={realtor2} name={"May Sia"} sold={500}/>
-            <Realtor image={realtor3} name={"Philip Bronzegate"} sold={150}/>
+            <div className="realtors__list">
+                <Realtor image={realtor1} name={"Erik Holsgate"} sold={300}/>
+                <Realtor image={realtor2} name={"May Sia"} sold={500}/>
+                <Realtor image={realtor3} name={"Philip Bronzegate"} sold={150}/>
+            </div>
         </section>
     );
 };
